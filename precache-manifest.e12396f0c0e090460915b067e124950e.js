727698a19997ab241ab376f62ed49e616e99e6df
nadiiabilockon <nbilokon@elitex.systems>
2020-01-15 17:15:54 +0200

    Updates

---
+++ precache-manifest.e12396f0c0e090460915b067e124950e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249ab45a6e0e6d892e83aa1a91bd3aa9",
+    "url": "/git_viewer/index.html"
+  },
+  {
+    "revision": "3859544113918d4e7d8e",
+    "url": "/git_viewer/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a2930f38ac4bced227f6",
+    "url": "/git_viewer/static/js/2.b24407d9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/git_viewer/static/js/2.b24407d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3859544113918d4e7d8e",
+    "url": "/git_viewer/static/js/main.ae565647.chunk.js"
+  },
+  {
+    "revision": "d18051246ef238098e1a",
+    "url": "/git_viewer/static/js/runtime-main.a5ab216a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/git_viewer/static/media/logo.5d5d9eef.svg"
+  }
+]);
